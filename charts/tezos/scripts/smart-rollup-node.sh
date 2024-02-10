51e27452125789a12b5c1e442c85bbd757df8dbc
Nicolas Ochem <nicolas.ochem@gmail.com>
2024-02-09 22:15:06 -0800

    use the smart rollup node config initializer

--- charts/tezos/scripts/smart-rollup-node.sh
+++ charts/tezos/scripts/smart-rollup-node.sh
@@ -9,12 +9,19 @@ ROLLUP_DATA_DIR_PREIMAGES="$ROLLUP_DATA_DIR/wasm_2_0_0"
 xxd -p -c 0 /usr/local/share/tezos/evm_kernel/evm_installer.wasm | tr -d '\n' > /var/tezos/smart-rollup-boot-sector
 mkdir -p "$ROLLUP_DATA_DIR_PREIMAGES"
 cp /usr/local/share/tezos/evm_kernel/* "$ROLLUP_DATA_DIR_PREIMAGES"
+ls -al $ROLLUP_DATA_DIR
+$TEZ_BIN/octez-smart-rollup-node \
+  --endpoint http://tezos-node-rpc:8732 \
+  -d $CLIENT_DIR \
+  init operator config for ${ROLLUP_ADDRESS} with operators ${OPERATORS_PARAMS} \
+  --data-dir ${ROLLUP_DATA_DIR} \
+  --boot-sector-file /var/tezos/smart-rollup-boot-sector \
+  --rpc-addr 0.0.0.0 \
+  --force
+
 CMD="$TEZ_BIN/octez-smart-rollup-node \
   --endpoint http://tezos-node-rpc:8732 \
   -d $CLIENT_DIR \
-  run operator for ${ROLLUP_ADDRESS} with operators ${OPERATORS_PARAMS} \
-  --data-dir ${ROLLUP_DATA_DIR} \
-  --boot-sector-file /var/tezos/smart-rollup-boot-sector \
-  --rpc-addr 0.0.0.0"
-
+  run \
+  --data-dir ${ROLLUP_DATA_DIR}"
 exec $CMD
