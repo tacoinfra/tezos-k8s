02f5b5b189962ba5966f0ffdb0bf84a2e5b636dc
Nicolas Ochem <nicolas.ochem@gmail.com>
2024-02-15 10:35:30 -0800

    do not import data from rollup node if already imported

--- charts/tezos/scripts/evm-sequencer.sh
+++ charts/tezos/scripts/evm-sequencer.sh
@@ -2,18 +2,19 @@ set -e
 
 TEZ_VAR=/var/tezos
 TEZ_BIN=/usr/local/bin
-CLIENT_DIR="$TEZ_VAR/client"
 ROLLUP_DATA_DIR="$TEZ_VAR/rollup"
 SEQUENCER_DATA_DIR="$TEZ_VAR/sequencer"
 
 set -x
 
-octez-evm-node init from rollup node ${ROLLUP_DATA_DIR} \
---data-dir ${SEQUENCER_DATA_DIR}
+if [ ! -e "/var/tezos/sequencer/store/store.1.mapping" ]; then
+  octez-evm-node init from rollup node ${ROLLUP_DATA_DIR} \
+  --data-dir ${SEQUENCER_DATA_DIR}
+fi
 
 
 CMD="$TEZ_BIN/octez-evm-node run sequencer \
-  with endpoint http://rollup-${MY_POD_NAME}:8932 \
+  with endpoint http://127.0.0.1:8932 \
   signing with edsk3rw6fcwjPe5xkGWAbSquLDQALKP8XyMhy4c6PQGr7qQKTYa8rX \
   --data-dir ${SEQUENCER_DATA_DIR} \
   --time-between-blocks 6 \
