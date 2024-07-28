7b83908aa9ec55e83d9c2e79f4f9732078da0fd0
Nicolas Ochem <nicolas.ochem@gmail.com>
2024-07-27 17:53:22 -0700

    TRD: remove lock automatically, do not upload to bucket, push slack alrt
    1. when run interactively, if the previous version of the program
       crashed, TRD asks you if you want to remove the lock. This causes
       problems with k8s cronjobs as they have to be fixed manually, shelled
       into to remove the lock. We remove the lock automatically. We already
       have a policy to forbid concurrent runs so the lock is useless
    2. we remove the bucket upload funcitonality which has been useless for
       us. Besides, it was using the snapshot engine container which no
       longer exists in the repo.
    3. we prevent the job from failing when TRD fails. Instead, we push a
       slack notification. This removes the need to clean up failed jobs
       within the cluster.

--- charts/tezos-reward-distributor/scripts/run.sh
+++ charts/tezos-reward-distributor/scripts/run.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
+# remove lock if present
+if [ -f /trd/cfg/lock ]; then
+  rm /trd/cfg/lock
+fi
 
 if [ "${DRY_RUN}" == "false" ]; then
   dry_run_arg=""
 else
   dry_run_arg="--dry_run"
-fi
-if [ "${ADJUSTED_EARLY_PAYOUTS}" == "false" ]; then
-  aep_arg=""
-else
-  aep_arg="--adjusted_early_payouts"
 fi
 python src/main.py \
   -M 2 \
@@ -20,5 +19,14 @@ python src/main.py \
   --initial_cycle ${INITIAL_CYCLE} \
   -N ${NETWORK} \
   ${EXTRA_TRD_ARGS} \
-  ${dry_run_arg} \
-  ${aep_arg}
+  ${dry_run_arg}
+
+# if TRD fails, send a slack alert
+if [ $? -ne 0 ]; then
+  # check if webhook is set
+  if [ -z "${SLACK_WEBHOOK}" ]; then
+    echo "TRD failed, but SLACK_WEBHOOK is not set, failing job"
+    exit 1
+  fi
+  curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Payout failed for $BAKER_ALIAS\"}" ${SLACK_WEBHOOK}
+fi
