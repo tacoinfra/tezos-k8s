9e8c78f7430503171739c72d47c1c3cadd2b6b99
Nicolas Ochem <nicolas.ochem@gmail.com>
2024-08-08 08:45:02 -0700

    Trd updates (#18)
    * TRD: remove lock automatically, do not upload to bucket, push slack alrt
    
    1. when run interactively, if the previous version of the program
       crashed, TRD asks you if you want to remove the lock. This causes
       problems with k8s cronjobs as they have to be fixed manually, shelled
       into to remove the lock. We remove the lock automatically. We already
       have a policy to forbid concurrent runs so the lock is useless
    2. we remove the bucket upload funcitonality which has been useless for
       us. Besides, it was using the snapshot engine container which no
       longer exists in the repo.
    3. we prevent the job from failing when TRD fails. Instead, we push a
       slack notification. This removes the need to clean up failed jobs
       within the cluster.
    
    * fix indent
    
    * use slack token instead of webhook
    
    replace slack webhook with bot token
    
    * use python for sending slack alert

--- charts/tezos-reward-distributor/scripts/run.sh
+++ charts/tezos-reward-distributor/scripts/run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
+# remove lock if present
+if [ -f /trd/cfg/lock ]; then
+  rm /trd/cfg/lock
+fi
 
 if [ "${DRY_RUN}" == "false" ]; then
   dry_run_arg=""
 else
   dry_run_arg="--dry_run"
 fi
-if [ "${ADJUSTED_EARLY_PAYOUTS}" == "false" ]; then
-  aep_arg=""
-else
-  aep_arg="--adjusted_early_payouts"
-fi
+
 python src/main.py \
   -M 2 \
   --reward_data_provider ${REWARD_DATA_PROVIDER} \
@@ -20,5 +20,43 @@ python src/main.py \
   --initial_cycle ${INITIAL_CYCLE} \
   -N ${NETWORK} \
   ${EXTRA_TRD_ARGS} \
-  ${dry_run_arg} \
-  ${aep_arg}
+  ${dry_run_arg}
+
+# if TRD fails, send a slack alert
+if [ $? -ne 0 ]; then
+  # check if bot token and channel are set
+  if [ -z "${SLACK_BOT_TOKEN}" ] || [ -z "${SLACK_CHANNEL}" ]; then
+    echo "TRD failed, but SLACK_BOT_TOKEN or SLACK_CHANNEL is not set, failing job"
+    exit 1
+  fi
+  python -c "
+import os
+import requests
+import json
+
+slack_bot_token = os.getenv('SLACK_BOT_TOKEN')
+slack_channel = os.getenv('SLACK_CHANNEL')
+baker_alias = os.getenv('BAKER_ALIAS')
+
+response = requests.post(
+    'https://slack.com/api/chat.postMessage',
+    headers={
+        'Authorization': f'Bearer {slack_bot_token}',
+        'Content-Type': 'application/json; charset=utf-8'
+    },
+    data=json.dumps({
+        'channel': slack_channel,
+        'text': f'TRD Payout failed for Tezos baker {baker_alias}'
+    })
+)
+
+if not response.json().get('ok'):
+    print('Failed to send Slack alert')
+    print(response.json())
+    exit(1)
+"
+  if [ $? -ne 0 ]; then
+    echo "Failed to send Slack alert"
+    exit 1
+  fi
+fi
