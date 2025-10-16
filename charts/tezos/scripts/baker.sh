04e133e1182deeb500c3efc3c279efad639101e2
Nicolas Ochem <nicolas.ochem@gmail.com>
2025-10-16 07:54:56 -0700

    switch to agnostic baker, accuser and VDF

--- charts/tezos/scripts/baker.sh
+++ charts/tezos/scripts/baker.sh
@@ -7,8 +7,6 @@ NODE_DIR="$TEZ_VAR/node"
 NODE_DATA_DIR="$TEZ_VAR/node/data"
 BAKER_EXTRA_ARGS_FROM_ENV=${BAKER_EXTRA_ARGS}
 
-proto_command="{{ .command_in_tpl }}"
-
 my_baker_account="$(sed -n "$(($BAKER_INDEX + 1))p" < /etc/tezos/baker-account )"
 
 if [ "${my_baker_account}" == "" ]; then
@@ -19,7 +17,7 @@ if [ "${my_baker_account}" == "" ]; then
   done
 fi
 
-per_block_vote_file=/etc/tezos/baker-config/${my_baker_account}-${proto_command}-per-block-votes.json
+per_block_vote_file=/etc/tezos/baker-config/${my_baker_account}-per-block-votes.json
 
 if [ ! -f "$per_block_vote_file" ]; then
   echo "Error: $per_block_vote_file not found" >&2
@@ -29,10 +27,9 @@ fi
 if [ "$(cat $per_block_vote_file)" == "null" ]; then
   cat << EOF
 You must pass per-block-votes (such as liquidity_baking_toggle_vote) in values.yaml, for example:
-protocols:
-- command: ${proto_command}
-  vote:
-    liquidity_baking_toggle_vote: "on"
+votes:
+  liquidity_baking_toggle_vote: "on"
+  adaptive_issuance_vote: "pass"
 EOF
   exit 1
 fi
@@ -47,7 +44,7 @@ if [ -f /etc/tezos/baker-config/${my_baker_account}_dal_node_rpc_url ]; then
 fi
 
 CLIENT="$TEZ_BIN/octez-client -d $CLIENT_DIR"
-CMD="$TEZ_BIN/octez-baker-$proto_command -d $CLIENT_DIR"
+CMD="$TEZ_BIN/octez-baker -d $CLIENT_DIR"
 
 # ensure we can run octez-client commands without specifying client dir
 ln -s /var/tezos/client /home/tezos/.tezos-client
--- charts/tezos/scripts/rpc-baker.sh
+++ charts/tezos/scripts/rpc-baker.sh
@@ -7,7 +7,7 @@ NODE_DIR="$TEZ_VAR/node"
 BAKER_EXTRA_ARGS_FROM_ENV=${BAKER_EXTRA_ARGS}
 mkdir -p $CLIENT_DIR
 
-per_block_vote_file=/etc/tezos/baker-config/${BAKER_NAME}-${PROTO_COMMAND}-per-block-votes.json
+per_block_vote_file=/etc/tezos/baker-config/${BAKER_NAME}-per-block-votes.json
 
 if [ ! -f "$per_block_vote_file" ]; then
   echo "Error: $per_block_vote_file not found" >&2
@@ -25,7 +25,7 @@ if [ "${DAL_NODE_RPC_URL}" != "" ]; then
 fi
 
 CLIENT="$TEZ_BIN/octez-client -d $CLIENT_DIR"
-CMD="$TEZ_BIN/octez-baker-${PROTO_COMMAND} -d $CLIENT_DIR"
+CMD="$TEZ_BIN/octez-baker -d $CLIENT_DIR"
 
 # ensure we can run octez-client commands without specifying client dir
 ln -s /var/tezos/client /home/tezos/.tezos-client
